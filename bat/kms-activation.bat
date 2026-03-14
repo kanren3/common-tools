f40d740728e3a0d226f1b30a33f0d56f36828a15
kanren3 <root@ruriwo.com>
2026-03-14 14:06:57 +0800

    refactor: update KMS activation script to use slmgr.vbs and correct key usage

--- bat/kms-activation.bat
+++ bat/kms-activation.bat
@@ -1,7 +1,12 @@
-:: https://learn.microsoft.com/en-us/windows-server/get-started/kms-client-activation-keys
 @title KMS Activation
-@cscript //nologo slmgr /upk
-@cscript //nologo slmgr /ipk M7XTQ-FN8P6-TTKYV-9D4CC-J462D
-@cscript //nologo slmgr /skms kms.03k.org
-@cscript //nologo slmgr /ato
+@cscript //nologo slmgr.vbs /upk
+
+:: https://learn.microsoft.com/en-us/windows-server/get-started/kms-client-activation-keys
+
+::[Windows 11 Pro] W269N-WFGWX-YVC9B-4J6C9-T83GX
+::[Windows 11 Enterprise LTSC] M7XTQ-FN8P6-TTKYV-9D4CC-J462D
+
+@cscript //nologo slmgr.vbs /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+@cscript //nologo slmgr.vbs /skms kms.03k.org
+@cscript //nologo slmgr.vbs /ato
 @pause
